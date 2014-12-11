bcbaed2a2cc120f23675e12e2e360bbc125a095f
antiproton <dr.urban@netreal.de>
2014-12-11 15:08:12 +0100

    ajax

--- js/ajax.js
+++ js/ajax.js
@@ -38,7 +38,9 @@ var request = false;
 			// wenn der readyState 4 und der request.status 200 ist, dann ist alles korrekt gelaufen
 			case 4:
 				if (request.status != 200) {
-					alert("Der Request wurde abgeschlossen, ist aber nicht OK\nFehler:"+request.status);
+					//alert("Der Request wurde abgeschlossen, ist aber nicht OK\nFehler:"+request.status);
+					setRequest(value);
+
 				} else {
 					 content = request.responseText;
 					// den Inhalt des Requests in das <div> schreiben
