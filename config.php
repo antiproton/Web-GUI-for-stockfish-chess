1dba694efbc819e6d73ae3dd026b3ad872aea5b5
antiproton <dr.urban@netreal.de>
2015-04-03 10:25:38 +0200

    configuration

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+#------------------------------------------------------------------------------------------#
+#  Copyright (c) Dr. R. Urban                                                              #
+#  03.04.2015                                                                              #
+#  Released under the MIT license                                                          #
+#  https://github.com/antiproton                                                           #
+#  http://www.genialschach.de/                                                             #
+#------------------------------------------------------------------------------------------#
+$security_code = "1234";
+#------------------------------------
+$pfad_stockfish  = "C:/xampp/htdocs/netreal/Stockfish/src/stockfish.exe";
+#$pfad_stockfish  = "/htdocs/netreal/Stockfish/src/stockfish";
+#------------------------------------
+$thinking_time = 500; # milliseconds
+#------------------------------------
+?>
